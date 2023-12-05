7774f1c74d2f57a7fd4b7aafbc3abd96cea07a4c
VanoKi <kirsvano@gmail.com>
2023-12-05 19:10:20 +0300

    getMaxNumber solved

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -41,8 +41,8 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  return Math.max(a, b, c);
 }
 
 /**
