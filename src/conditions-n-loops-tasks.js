1a82756cfed56522a8a1357ba95110cba054ac29
VanoKi <kirsvano@gmail.com>
2024-01-06 20:34:35 +0300

    solved convertNumberToString

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -128,27 +128,53 @@ function convertToRomanNumerals(/* num */) {
 
 /* prettier-ignore */
 function convertNumberToString(numberStr) {
-  const dict = {
-    '0': 'zero',
-    '1': 'one',
-    '2': 'two',
-    '3': 'three',
-    '4': 'four',
-    '5': 'five',
-    '6': 'six',
-    '7': 'seven',
-    '8': 'eight',
-    '9': 'nine',
-    '-': 'minus',
-    '.': 'point',
-    ',': 'point'
+  let ans = '';
+  for (let i = 0; i < numberStr.length; i += 1) {
+    switch (numberStr[i]) {
+      case "1":
+        ans += ' one';
+        break;
+      case "2":
+        ans += ' two';
+        break;
+      case "3":
+        ans += ' three';
+        break;
+      case "4":
+        ans += ' four';
+        break;
+      case '5':
+        ans += ' five'
+        break
+      case "6":
+        ans += ' six';
+        break;
+      case "7":
+        ans += ' seven';
+        break;
+      case "8":
+        ans += ' eight';
+        break;
+      case "9":
+        ans += ' nine';
+        break;
+      case "0":
+        ans += ' zero';
+        break;
+      case '-':
+        ans += 'minus'
+        break;
+      case '.':
+        ans += ' point';
+        break
+      case ',':
+        ans += ' point';
+        break
+      default:
+        break
+      }
   }
-  const ans = [];
-  // eslint-disable-next-line no-restricted-syntax
-  for (const i of numberStr) {
-    ans.push(dict[i]);
-  }
-  return ans.join(' ');
+  return ans.trim();
 }
 
 /**
